7842f29b0874b73ab1050bf7e84f0b2e860fac4d
huzhaoliang <zhaoliang.hu@yahoo.com>
2017-01-11 17:49:18 +0800

    update version

---
+++ KingsDemoNoStoryBoard/HomeNavigationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeNavigationViewController.swift
+//  KingsDemoNoStoryBoard
+//
+//  Created by HuZhaoLiang on 2017/1/11.
+//  Copyright © 2017年 leonchan. All rights reserved.
+//
+
+import Foundation
