b60edbbecf693076cf4c0ff1d836f02e5765af50
huzhaoliang <zhaoliang.hu@yahoo.com>
2017-01-15 00:20:51 +0800

    aa

--- KingsDemoNoStoryBoard/SearchPageView.swift
+++ KingsDemoNoStoryBoard/SearchPageView.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class SearchPageView: UIView {
+class SearchPageView: UIView
+{
 
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
+    override func draw(_ rect: CGRect)
+    {
         // Drawing code
     }
 
